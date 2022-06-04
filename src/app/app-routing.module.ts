cfdaeb836defa21666cc5b3a963eceea721614bc
AIR <grvcreatives@GRVs-MacBook-Air.local>
2022-06-04 16:40:04 +0000

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+
+  { path: '',   redirectTo: '/', pathMatch: 'full' }, // redirect to `first-component`
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
